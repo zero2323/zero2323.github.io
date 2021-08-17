6fb9eec44c90cff8d7e6b1d34cf496d971ffce85
zero2323 <ramzibrahimovich@gmail.com>
2021-08-17 14:23:59 +0100

    second

---
+++ assets/loader.js
@@ -0,0 +1,96 @@
+var fill_with_script = true;
+
+var Devoloper = "Zeghida Ramzi";
+var Profession = "Devoloper and Designer";
+var About = `Hello i'am <b>Zeghida Ramzi<b> a computer science graduate whith the passion of anything cyber i studied network security and prgraing languages , which make me qualified 
+for creating,designing and devoloping computer or mobile software `;
+var big_pic = "profile.jpg";
+var profile = "profile.jpg";
+var contact = {
+    email: "zeghidaramzi@gmail.com",
+    phone: "+111111111",
+    facebook: "zeghidaramzi@gmail.com",
+    twitter: "zeghidaramzi@gmail.com"
+}
+var expertise = {
+    0: {
+        title: "Bachelor's degree",
+        date: "2014-2015",
+        description: `Mokawma high school / Technical Math -Mechanical Engineering
+            Annba Algeria`
+    },
+    1: {
+        title: "License's degree",
+        date: "2015-2018",
+        description: `University Badji Mokhtar Annaba
+        <ul>
+        <li>Backup software with the functionality of
+        encryption and deference comparison</li>
+        </ul>`
+    },
+    2: {
+        title: "Master's degree",
+        date: "2018-2020",
+        description: `University Badji Mokhtar Annaba
+        <ul>
+        <li>Encryption/Decryption System based on the
+        structure of DNA</li>
+        </ul>`
+    }
+};
+
+var languages = {
+    0: {
+        name: "Arabic",
+        img: "ae.png",
+        rate: 5
+    },
+    1: {
+        name: "french",
+        img: "fr.png",
+        rate: 3
+    },
+    2: {
+        name: "english",
+        img: "us.png",
+        rate: 4
+    }
+};
+
+var skills = {
+    0: {
+        name: "secure network admin",
+        img: "nodejs.png",
+        rate: 3
+    },
+    1: {
+        name: "php",
+        img: "php.png",
+        rate: 4
+    },
+    2: {
+        name: "html",
+        img: "html.png",
+        rate: 5
+    },
+    3: {
+        name: "css",
+        img: "css.png",
+        rate: 3
+    },
+    4: {
+        name: "java",
+        img: "java.png",
+        rate: 4
+    },
+    5: {
+        name: "python",
+        img: "nodejs.png",
+        rate: 4
+    },
+    6: {
+        name: "javascript",
+        img: "javascript.png",
+        rate: 3
+    }
+};
--- assets/main.js
+++ assets/main.js
@@ -1,100 +1,3 @@
-var fill_with_script = true;
-
-var Devoloper = "Zeghida Ramzi";
-var Profession = "Devoloper and Designer";
-var About = `Hello i'am <b>Zeghida Ramzi<b> a computer science graduate whith the passion of anything cyber i studied network security and prgraing languages , which make me qualified 
-for creating,designing and devoloping computer or mobile software `;
-var big_pic = "profile.jpg";
-var profile = "profile.jpg";
-var contact = {
-    email: "zeghidaramzi@gmail.com",
-    phone: "+111111111",
-    facebook: "zeghidaramzi@gmail.com",
-    twitter: "zeghidaramzi@gmail.com"
-}
-var expertise = {
-    0: {
-        title: "Bachelor's degree",
-        date: "2014-2015",
-        description: `Mokawma high school / Technical Math -Mechanical Engineering
-            Annba Algeria`
-    },
-    1: {
-        title: "License's degree",
-        date: "2015-2018",
-        description: `University Badji Mokhtar Annaba
-        <ul>
-        <li>Backup software with the functionality of
-        encryption and deference comparison</li>
-        </ul>`
-    },
-    2: {
-        title: "Master's degree",
-        date: "2018-2020",
-        description: `University Badji Mokhtar Annaba
-        <ul>
-        <li>Encryption/Decryption System based on the
-        structure of DNA</li>
-        </ul>`
-    }
-};
-
-var languages = {
-    0: {
-        name: "Arabic",
-        img: "ae.png",
-        rate: 5
-    },
-    1: {
-        name: "french",
-        img: "fr.png",
-        rate: 3
-    },
-    2: {
-        name: "english",
-        img: "us.png",
-        rate: 4
-    }
-};
-
-var skills = {
-    0: {
-        name: "Nodejs",
-        img: "nodejs.png",
-        rate: 3
-    },
-    1: {
-        name: "php",
-        img: "php.png",
-        rate: 4
-    },
-    2: {
-        name: "html",
-        img: "html.png",
-        rate: 5
-    },
-    3: {
-        name: "css",
-        img: "css.png",
-        rate: 3
-    },
-    4: {
-        name: "java",
-        img: "java.png",
-        rate: 4
-    },
-    5: {
-        name: "python",
-        img: "nodejs.png",
-        rate: 4
-    },
-    6: {
-        name: "javascript",
-        img: "javascript.png",
-        rate: 3
-    }
-};
-
 Object.size = function(obj) {
     var size = 0,
         key;
@@ -169,6 +72,7 @@ function show_loader() {
     $('.loader').show();
     $(document).ready(function() {
         if (fill_with_script) {
+            window.document.title = Devoloper + " | " + Profession;
             $('.p_header h1').html(Devoloper);
             $('.p_header span').html(Profession);
             $('.descr-text').html(About);
